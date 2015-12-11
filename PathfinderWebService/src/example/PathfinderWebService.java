a73d9055804797b393538b9ba8fb686fa7a41b83
Andrew Lang <msufan9825@gmail.com>
2015-12-11 15:57:49 -0500

    added the getFloor method, added JavaDoc documentation for all methods

--- PathfinderWebService/src/example/PathfinderWebService.java
+++ PathfinderWebService/src/example/PathfinderWebService.java
@@ -20,18 +20,12 @@ public class PathfinderWebService {
     private static String DB_LOGIN_ID = "postgres";
     private static String DB_PASSWORD = "postgres";
 
-    @GET
-    @Path("/")
-    // The Java method will produce content identified by the MIME Media type "text/plain"
-    @Produces("text/plain")
-    public String getWelcomeMessage() {
-        // Return some cliched textual content
-        return "Welcome to the Pathfinder Database";
-    }
-
     /**
-     * @param id a building ID in the pathfinder database
-     * @return a string version of the building record, if any, with the given id
+     * getBuilding outputs a String containing the row of the Building table 
+     * specified by the given id.
+     * 
+     * @param: id, a building ID in the pathfinder database
+     * @return: a String version of the building record, if any, with the given id
      */
     @GET
     @Path("/building/{id}")
@@ -46,7 +40,7 @@ public class PathfinderWebService {
             if (resultSet.next()) {
                 result ="" + resultSet.getInt(1) + " " + resultSet.getString(2) + " " + resultSet.getFloat(3) + " " + resultSet.getFloat(4);
             } else {
-                result = "nothing found...";
+                result = "null";
             }
             resultSet.close();
             statement.close();
@@ -57,6 +51,13 @@ public class PathfinderWebService {
         return result;
     }
 
+    /**
+     * getBuildings pulls the entire Building table from the pathfinder database
+     * and outputs it as plaintext
+     * 
+     * @return: a String containing the Building table, with each value separated by 
+     *          commas and each row separated by new lines
+     */
     @GET
     @Path("/buildings")
     @Produces("text/plain")
@@ -78,7 +79,41 @@ public class PathfinderWebService {
         }
         return result;
     }
+    
+    /**
+     * getFloor returns a String containing a row of the Floor table 
+     * based on the given buildingID and floorID
+     * 
+     * @param: buildingID, the BuildingID for the query
+     * @param: floorID, the FloorID for the query
+     * 
+     * @return: a String giving the information in the row of the Floor table that was queried
+     */
+    @GET
+    @Path("/building/{buildingID}/floor/{floorID}")
+    @Produces("text/plain")
+    public String getFloor(@PathParam("buildingID") int buildingID, @PathParam("floorID") int floorID) {
+        String result;
+        try {
+            Class.forName("org.postgresql.Driver");
+            Connection connection = DriverManager.getConnection(DB_URI, DB_LOGIN_ID, DB_PASSWORD);
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Floor WHERE Floor.BuildingID=" + buildingID + "AND Floor.FloorID=" + floorID);
+            if (resultSet.next()) {
+                result ="" + resultSet.getInt(1) + " " + resultSet.getInt(2) + " " + resultSet.getInt(3) + " " + resultSet.getString(4);
+            } else {
+                result = "null";
+            }
+            resultSet.close();
+            statement.close();
+            connection.close();
+        } catch (Exception e) {
+            result = e.getMessage();
+        }
+        return result;
+    }
 
+    //create the web service and run it
     public static void main(String[] args) throws IOException {
         HttpServer server;
         try {
